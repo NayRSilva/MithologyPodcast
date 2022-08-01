9e08e435b0d632a40262ee626ab81a914f19dc4f
NayRSilva <nayararossisilva@gmail.com>
2022-08-01 19:21:37 -0300

    testing

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -2,10 +2,28 @@ module.exports = {
     onPreBuild: ({utils}) =>{
         let currentProject= "mythology"
         let projectChanged= false
+        let lastDeployCommited = process.env.CACHED_COMMIT_REF;
+        const latestCommit = 'HEAD';
+        projectChanged = projectChanged(currentProject, lastDeployCommited, latestCommit)
+
         if(!projectChanged){
             utils.build.cancelBuild("Build was cancelled because ${currentProject} was not changed")
 
         }
     }
+
+}
+
+projectChanged = (currentProject, fromHash, toHash)=>{
+    const execSync = require('child_process').execSync;
+    const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
+  const output = execSync(getAffected).toString();
+  //get the list of changed projects from the output
+  const changedProjects = JSON.parse(output).projects;
+  if (changedProjects.find(project => project === currentProject)) {
+    return true;
+  } else {
+    return false;
+  }
 
 }
