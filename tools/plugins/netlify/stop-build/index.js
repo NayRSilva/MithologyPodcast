c5579b9e208d3702d2b7080f357fee6f4650e833
NayRSilva <nayararossisilva@gmail.com>
2022-08-01 19:43:55 -0300

    try

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,24 +1,27 @@
-function projectChanged  (currentProject, fromHash, toHash){
-    const execSync = require('child_process').execSync;
-    const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
-  const output = execSync(getAffected).toString();
-  //get the list of changed projects from the output
-  const changedProjects = JSON.parse(output).projects;
-  if (changedProjects.find(project => project === currentProject)) {
-    return true;
-  } else {
-    return false;
-  }
 
-}
 
 module.exports = {
     onPreBuild: ({utils}) =>{
+
+        let projectChangedf = (currentProject, fromHash, toHash)=>{
+            const execSync = require('child_process').execSync;
+            const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
+          const output = execSync(getAffected).toString();
+          //get the list of changed projects from the output
+          const changedProjects = JSON.parse(output).projects;
+          if (changedProjects.find(project => project === currentProject)) {
+            return true;
+          } else {
+            return false;
+          }
+        
+        }
+
         let currentProject= "mythology"
         let projectChanged= false
         let lastDeployCommited = process.env.CACHED_COMMIT_REF;
         const latestCommit = 'HEAD';
-        projectChanged = projectChanged(currentProject,lastDeployCommited, latestCommit)
+        projectChanged = projectChangedf(currentProject,lastDeployCommited, latestCommit)
 
         if(!projectChanged){
             utils.build.cancelBuild("Build was cancelled because ${currentProject} was not changed")
@@ -26,5 +29,6 @@ module.exports = {
         }
     }
 
+   
 }
 
