11c9d3687a5f894cfd5dc892e9360b52aaa5876a
NayRSilva <nayararossisilva@gmail.com>
2022-08-10 01:12:06 -0300

    Creating participant section

---
+++ apps/mythology/src/components/ParticipantSection.tsx
@@ -0,0 +1,70 @@
+import { useCallback, useState } from "react";
+import ControlledSpotify from "./ControlledSpotify";
+import EpisodeMidia from "./EpisodeMidia";
+import ParticipantCard from "./ParticipantCard";
+import { BoldTitle, CustomSecondTilte, GridDiv, LateralSliderDiv, LightTitle, Titles} from "./styles/componentStyles";
+
+type ESectionProps = {
+    id?: string|undefined;
+    children?: JSX.Element[] | JSX.Element;
+}
+
+export function ParticipantSection(props: ESectionProps) {
+    const [width, setWidth] = useState(0);
+    const [cardWidth, setCardWidth] = useState(0);
+
+    const size=5;
+    const cardsWidth = size * cardWidth;
+
+    const measuredRef = useCallback(node => {
+      if (node !== null) {
+        setWidth(node.getBoundingClientRect().width);
+      }
+    }, []);
+
+    const measuredCard = useCallback(node => {
+        if (node !== null) {
+          setCardWidth(node.getBoundingClientRect().width);
+        }
+      }, []);
+    const isSmallerThenContainer= ()=>{
+            if(cardsWidth<width){
+              console.log("smaller ", width)
+                return true;
+
+            }          
+            console.log("noy small", width, cardsWidth, cardWidth)   
+            return false
+    }
+    return (
+      <section ref={measuredRef} >
+        <CustomSecondTilte>
+            Participantes
+        </CustomSecondTilte>
+        <LateralSliderDiv tabIndex={0} className={(isSmallerThenContainer())? 'center-small':''}>
+
+            <div ref={measuredCard}>
+                <ParticipantCard></ParticipantCard>
+
+            </div>
+            <ParticipantCard></ParticipantCard>
+            <ParticipantCard></ParticipantCard>
+            <ParticipantCard></ParticipantCard>
+
+
+
+            <ParticipantCard></ParticipantCard>
+
+
+            
+
+        </LateralSliderDiv>
+        
+
+
+      </section>
+    );
+  }
+  
+  export default ParticipantSection;
+  
--- apps/mythology/src/components/styles/componentStyles.ts
+++ apps/mythology/src/components/styles/componentStyles.ts
@@ -13,8 +13,17 @@ export const CommonDiv = styled.div`
     display:flex;
     flex-wrap: wrap;
     justify-content:center;
-`
 
+    
+`
+export const ParticipantContainer= styled.div`
+    flex-direction: column;
+    display:flex;
+    text-align:center;
+    flex-shrink:0;
+    width:200px;
+
+`
 export const RedBox = styled.div`
     height: 50px;
     width: 100%;
@@ -64,3 +73,34 @@ export const EpisodeInfo= styled.p`
 
 `
 
+export const CustomSecondTilte = styled.h2`
+    font-family: 'FiraSansCondensed';
+    font-size: 1.5em;
+    letter-spacing: -2px;
+
+`
+
+
+export const LateralSliderDiv = styled.div`
+    min-height: 200px;
+    display:flex;
+    flex: 0 0 100%;
+    width:100%;
+    justify-content:flex-start;
+    overflow-x:auto;
+    padding: 2% 0;
+
+    &:focus{
+        border: 2px solid black;
+    }
+
+ 
+`
+
+export const ParticipantPhoto = styled.img`
+    width: 80%;
+    margin: 10% 10%;
+    border-radius: 50%;
+    flex-shrink:0;
+
+`
