4579ac811acbbc9b5f30c10d2feebf7355c6d300
Lucas Vinicius <lucasvini1298@gmail.com>
2022-08-13 16:43:44 -0300

    ajusta posicionamento do header

--- apps/mythology/src/feature/Navbar.tsx
+++ apps/mythology/src/feature/Navbar.tsx
@@ -1,17 +1,18 @@
 import { url } from "inspector";
 import { NavLink } from "react-router-dom";
+import { apiProvider } from "../services/api/ApiProvider"
 
   // Easiest way to declare a Function Component; return type is inferred.
   const Navbar = () => {
-
     return(
-        <div style={HeaderStyle}>
-          <NavLink style={HeaderLinkStyle}
-            to="/home">
-            <div style={ImgHeaderStyle}/>
-          </NavLink>
-        </div>
+      <div style={HeaderStyle}>
+        <NavLink
+          to="/home">
+          <img src='../assets/img/header.png' height='100%' width='fit'/>
+        </NavLink>
+      </div>
     )
+    return null;
   }
 
   const HeaderStyle={
@@ -20,18 +21,9 @@ import { NavLink } from "react-router-dom";
     backgroundColor: '#E45C3F',
     display:'flex',
     justifyContent: 'center',
-    alignItens: 'stretch'
   }
   
-  const HeaderLinkStyle = {
-    width: '20%',
-    height: '90%'
-  }
-
   const ImgHeaderStyle = {
-    backgroundImage: 'url("https://www.teclasap.com.br/wp-content/uploads/2014/11/banner.png")',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
     width: '100%',
     height: '100%'
   }
--- apps/mythology/src/services/api/ApiProvider.tsx
+++ apps/mythology/src/services/api/ApiProvider.tsx
@@ -3,25 +3,22 @@ import axios from 'axios';
 import {useQuery} from 'react-query'
 
 
-const baseURL = 'http://localhost:1337/api/'
+const baseURL = 'api.mitologianodiaadia.com.br/api/'
 
 const getFunction = (resource:string)=>{
     return axios.get(baseURL+ resource+'?populate=*')
-  
   }
 
 const GetSingleAll = (resource:string, nameKey:string) => {
   return useQuery([nameKey], ()=> getFunction(resource))
-
-
 }
+
 const getEpisodesParticipants = ()=>{
   return axios.get(baseURL+ 'episodes?populate=%2A&populate[0]=Participante&populate[1]=Participante.imagem')
-
 }
+
 const GetEpisodeWithParticipants=()=>{
   return useQuery(['episodes'], ()=> getEpisodesParticipants())
-
 }
 
 const getEmbed= (url:string)=>{
@@ -30,11 +27,21 @@ const getEmbed= (url:string)=>{
 
 const GetQueryOEmb= (url:string)=>{
   return useQuery(['episodes'], ()=> getEmbed(url))
+}
 
+const baseURLalt = 'api.mitologianodiaadia.com.br/api/';
+
+const LogoImage= ()=>{
+  return axios.get(baseURL + 'Logo?populate=%2A')
+}
+
+const GetLogoImage = () => {
+  return useQuery(['logoIMG'], ()=> LogoImage())
 }
 
 export const apiProvider = { 
-   GetSingleAll,
-   GetQueryOEmb,
-   GetEpisodeWithParticipants
-  };
+  GetSingleAll,
+  GetQueryOEmb,
+  GetEpisodeWithParticipants,
+  GetLogoImage
+};
