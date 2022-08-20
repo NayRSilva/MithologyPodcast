4d45c8adba84a9b78eb7011e68c7f596ef22d294
NayRSilva <nayararossisilva@gmail.com>
2022-08-20 01:18:52 -0300

    improving baseUrl

--- apps/mythology/src/app/app.tsx
+++ apps/mythology/src/app/app.tsx
@@ -3,28 +3,27 @@ import styles from './app.module.css';
 import Global from '../components/styles/global';
 import Home from '../feature/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {QueryClientProvider, QueryClient} from 'react-query';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import Episode from '../feature/Episode';
 import Navbar from '../feature/Navbar';
+import { apiProvider } from '../services/api/ApiProvider';
 
-const queryClient = new QueryClient;
+const queryClient = new QueryClient();
 
 export function App() {
+  apiProvider.useDefineUrl();
   return (
     <>
-    <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Navbar></Navbar>
-          <Global/>
+          <Global />
           <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/episode/:id" element={<Episode />}/>
-
-
-            </Routes>
-
-          </BrowserRouter>
-          </QueryClientProvider>
+            <Route path="/" element={<Home />} />
+            <Route path="/episode/:id" element={<Episode />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
       {/* <NxWelcome title="mythology" /> */}
     </>
   );
