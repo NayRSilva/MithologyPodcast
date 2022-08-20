1e20863b0e1b7c8721906c0c069aeed615897109
NayRSilva <nayararossisilva@gmail.com>
2022-08-20 01:20:24 -0300

    cleaning unused variables

--- apps/mythology/src/components/TextBox.tsx
+++ apps/mythology/src/components/TextBox.tsx
@@ -1,19 +1,12 @@
-import { TranscriptionBox } from "./styles/componentStyles";
+import { TranscriptionBox } from './styles/componentStyles';
 
 type ContainerProps = {
-    children?: JSX.Element[] | JSX.Element;
-}
+  children?: JSX.Element[] | JSX.Element;
+};
 
 export function TextBox(props: ContainerProps) {
+  // const transcribe = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. "
+  return <TranscriptionBox tabIndex={0}>{props.children}</TranscriptionBox>;
+}
 
-    const transcribe = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. "
-    return (
-      
-      <TranscriptionBox tabIndex={0}>
-        {props.children}
-      </TranscriptionBox>
-    );
-  }
-  
-  export default TextBox;
-  
+export default TextBox;
