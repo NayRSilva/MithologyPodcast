3e651283ea24ad88f8276afc1ff01ca2709a1c07
NayRSilva <nayararossisilva@gmail.com>
2022-08-21 20:19:57 -0300

    modifying navbar to stop overscroll

--- apps/mythology/src/feature/Navbar.tsx
+++ apps/mythology/src/feature/Navbar.tsx
@@ -1,48 +1,52 @@
-import { url } from "inspector";
-import { NavLink } from "react-router-dom";
-import { apiProvider } from "../services/api/ApiProvider"
+import { url } from 'inspector';
+import { NavLink } from 'react-router-dom';
+import { apiProvider } from '../services/api/ApiProvider';
 
-  // Easiest way to declare a Function Component; return type is inferred.
-  const Navbar = () => {
-    const {isLoading, data} = apiProvider.GetLogoImage();
-    let dataImg: string;
+// Easiest way to declare a Function Component; return type is inferred.
+const Navbar = () => {
+  const { isLoading, data } = apiProvider.useGetLogoImage();
+  let dataImg: string;
 
+  if (!isLoading) {
+    dataImg = data.data.data.attributes.LogoImage.data.attributes.url;
 
-    if(!isLoading){
-      dataImg = data.data.data.attributes.LogoImage.data.attributes.url;
-      
-      return(
-        <div style={HeaderStyle}>
-          <NavLink
-            to="/home">
-            <img alt='logo mitologia podcast' src={dataImg ? dataImg : '../assets/image/header.png'} height='100%'/>
-          </NavLink>
-        </div>
-      )
-    }
+    return (
+      <div style={HeaderStyle}>
+        <NavLink to="/home">
+          <img
+            alt="logo mitologia podcast"
+            src={dataImg ? dataImg : '../assets/image/header.png'}
+            height="100%"
+          />
+        </NavLink>
+      </div>
+    );
+  }
 
-  return(
+  return (
     <div style={HeaderStyle}>
-      <NavLink
-        to="/home">
-        <img alt='logo mitologia podcast' src='../assets/image/header.png' height='100%'/>
+      <NavLink to="/home">
+        <img
+          alt="logo mitologia podcast"
+          src="../assets/image/header.png"
+          height="100%"
+        />
       </NavLink>
     </div>
   );
-}
+};
 
-  const HeaderStyle={
-    width: '100vw',
-    height: '13vh',
-    backgroundColor: '#E45C3F',
-    display:'flex',
-    justifyContent: 'center',
-  }
-  
-  const ImgHeaderStyle = {
-    width: '100%',
-    height: '100%'
-  }
+const HeaderStyle = {
+  width: '100%',
+  height: '13vh',
+  backgroundColor: '#E45C3F',
+  display: 'flex',
+  justifyContent: 'center',
+};
 
-  export default Navbar;
+const ImgHeaderStyle = {
+  width: '100%',
+  height: '100%',
+};
 
+export default Navbar;
