bd6854ffc70553b4445080442227c56363d00c6e
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2022-09-01 11:15:58 -0300

    Add global links to strapi

---
+++ apps/api/src/api/links-globais/routes/links-globais.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * links-globais router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::links-globais.links-globais');
